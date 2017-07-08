349fc86dfe98bc23db7b97194fb7373322a61f9a
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-07-08 18:41:06 -0300

    add movement to the character

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private Rigidbody2D rigidBody;
+	private Animator animator;
+
+	[SerializeField]
+	private float movementSpeed;
+	private float countDownTimer = 3.0f;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		StartCoroutine("Countdown", countDownTimer);
+	}
+
+	void FixedUpdate() {
+		HandleMovement();
+	}
+
+	private void HandleMovement() {
+		rigidBody.velocity = new Vector2(movementSpeed, rigidBody.velocity.y);
+	}
+
+	private IEnumerator Countdown(int time) {
+		while (countDownTimer > 0) {
+			yield return new WaitForSeconds(1);
+			countDownTimer -= 1;
+		}
+
+		animator.SetBool("running", true);
+	}
+}
