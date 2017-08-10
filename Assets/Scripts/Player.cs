4e649075518f0ac41d2645be657d7a4bfd5c71bd
Gabriel Rascovit <rascovit@gmail.com>
2017-08-10 00:44:08 -0300

    add jump action to the character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,16 @@ public class Player : MonoBehaviour {
 	private float movementSpeed;
 	private float countDownTimer = 3.0f;
 	private bool sliding;
+    [SerializeField]
+    private Transform[] groundPoints;
+    [SerializeField]
+    private float groundRadius;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool isGrounded;
+    private bool jumping;
+    [SerializeField]
+    private float jumpForce;
 
 	void Start() {
 		rigidBody = GetComponent<Rigidbody2D>();
@@ -22,16 +32,28 @@ public class Player : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
+        isGrounded = IsGrounded();
 		HandleMovement();
-		ResetValues();
+        HandleLayers();
+        ResetValues();
 	}
 
 	private void HandleMovement() {
 		AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
+        if (rigidBody.velocity.y < 0) {
+            animator.SetBool("landing", true);
+        }
+
 		if (animator.GetBool ("running")) {
 			rigidBody.velocity = new Vector2 (movementSpeed, rigidBody.velocity.y);
 		}
+
+        if (isGrounded && jumping) {
+            isGrounded = false;
+            rigidBody.AddForce(new Vector2(0, jumpForce));
+            animator.SetBool("jumping", true);
+        }
 
 		if (sliding && !animatorStateInfo.IsName("Slide")) {
 			animator.SetBool("sliding", true);
@@ -44,7 +66,11 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey(KeyCode.DownArrow)) {
 			sliding = true;
 		}
-	}
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            jumping = true;
+        }
+    }
 
 	private IEnumerator Countdown(int time) {
 		while (countDownTimer > 0) {
@@ -57,5 +83,32 @@ public class Player : MonoBehaviour {
 
 	private void ResetValues() {
 		sliding = false;
+        jumping = false;
 	}
+
+    private bool IsGrounded() {
+        if (rigidBody.velocity.y <= 0) {
+            foreach (Transform point in groundPoints) {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++) {
+                    if (colliders[i].gameObject != gameObject) {
+                        animator.SetBool("jumping", false);
+                        animator.SetBool("landing", false);
+                        return true; 
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void HandleLayers() {
+        if (!isGrounded) {
+            animator.SetLayerWeight(1, 1);
+        } else {
+            animator.SetLayerWeight(1, 0);
+        }
+    }
 }
